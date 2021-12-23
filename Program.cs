aaecc057b8d4b305b3d0067a1bde34fbdd31ae6d
REL1980 <rafaelandeau@hotmail.com>
2021-12-23 11:52:32 -0300

    Third Commit

--- Program.cs
+++ Program.cs
@@ -9,4 +9,7 @@ var user = new User("Enrique","Diaz");
 
 Console.WriteLine(user.Fullname());
 
+Console.WriteLine(user.Name);
+user.Name = "Facundo";
+Console.WriteLine($"Luego del cambio de nombre, queda: {user.Name}");
 //36 minutos
